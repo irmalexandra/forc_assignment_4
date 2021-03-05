011dacd636ad36f779215af390b88caa491ea9f2
Emil Örn <emmikall95@gmail.com>
2021-03-05 14:26:46 +0000

    yes

---
+++ Classes/InputHandler.h
@@ -0,0 +1,16 @@
+//
+// Created by emmik on 05/03/2021.
+//
+
+#ifndef FORC_PA_4_INPUTHANDLER_H
+#define FORC_PA_4_INPUTHANDLER_H
+
+
+class InputHandler {
+    void createCharacter();
+    void saveCharacter();
+    void loadCharacter();
+};
+
+
+#endif //FORC_PA_4_INPUTHANDLER_H
