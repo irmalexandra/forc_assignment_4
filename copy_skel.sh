405ea4e4bdc248db98fe7aef6128a2a0db0e4073
Loki Alexander Hopkins <lokih@ru.is>
2021-03-10 17:22:56 +0000

    created shell script

---
+++ copy_skel.sh
@@ -0,0 +1,11 @@
+scp *.cpp lokih@skel.ru.is:~/cpp/PA3/
+scp Classes/*.cpp lokih@skel.ru.is:~/cpp/PA3/Classes
+scp Classes/*.h lokih@skel.ru.is:~/cpp/PA3/Classes
+scp Helpers/*.cpp lokih@skel.ru.is:~/cpp/PA3/Helpers
+scp Helpers/*.h lokih@skel.ru.is:~/cpp/PA3/Helpers
+scp Models/*.cpp lokih@skel.ru.is:~/cpp/PA3/Helpers
+scp Models/*.h lokih@skel.ru.is:~/cpp/PA3/Helpers
+scp Templates/*.cpp lokih@skel.ru.is:~/cpp/PA3/Helpers
+scp Templates/*.h lokih@skel.ru.is:~/cpp/PA3/Helpers
+scp Resources/*.txt lokih@skel.ru.is:~/cpp/PA3/
+
